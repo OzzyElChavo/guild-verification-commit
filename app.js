ac85358431b8c7ad4d7f7ceff055244f9f65d72d
OzzyElChavo <127722186+OzzyElChavo@users.noreply.github.com>
2025-10-31 02:21:17 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T23:21:13.427Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-10-30T23:21:14.830Z
+Commit: Fix CSS styling bug on mobile devices
